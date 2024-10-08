ef4d5eb100e58857e930d2317b3e22c180c16fd6
glycelyvon <glycel.virtucio@gmail.com>
2024-10-08 21:55:25 +0800

    "Refactored game logic, removed PHP file, and updated HTML and CSS files."

--- game.php
+++ game.php
@@ -33,4 +33,6 @@ $result = mysqli_query($conn, $sql);
 
 if (mysqli_num_rows($result) > 0) {
     while($row = mysqli_fetch_assoc($result)) {
-        echo "Player: " . $row["player_name"].
+        echo "Player: " . $row["player_name"];
+    }
+}
